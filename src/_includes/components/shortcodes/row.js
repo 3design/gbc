6c5a9711585a6ee164ad4a5fc5451e6b1dc03e81
3design <3design@users.noreply.github.com>
2024-08-09 15:04:50 +0000

    deploy: 0360d8ff51a8b87b747bf4ce4f4356be241a8cc9

--- src/_includes/components/shortcodes/row.js
+++
@@ -1,7 +0,0 @@
-module.exports = (content) => {
-    return `<section class="bg-c-bg-l/50">
-            <div class="mx-5">
-            ${content}
-            </div>
-            </section>`
-};
--- src/_includes/components/shortcodes/shortcode.js
+++
@@ -1,5 +0,0 @@
-const row= require("./row.js");
-
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPairedShortcode("row", row);
-};
